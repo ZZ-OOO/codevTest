9e5d98546593fccb14f97a1b37301a727e4a5e41
ZZ-OOO <wngns903@gmail.com>
2022-10-10 20:15:30 +0900

    Initial commit

---
+++ src/main/java/com/codev/CodevApplication.java
@@ -0,0 +1,13 @@
+package com.codev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodevApplication.class, args);
+	}
+
+}
